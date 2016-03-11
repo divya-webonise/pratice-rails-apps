7ab0e2c3b1571f0c04531c1fdc99b949d9c6ee8a
divya sharma <divya.sharma@weboniselab.com>
2016-03-11 18:17:46 +0530

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'a469a5d596aa37586c8fafce9d025f49dc5e6d2b49beed11762672e72c3f901c156d836c89815467b9b0b5c01e7077e4dcfb0ff757bd97fcb7d536e9c11de397'
